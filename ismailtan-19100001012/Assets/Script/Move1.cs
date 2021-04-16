92ec235f5186d513fdd50b8b475923b67590030b
IsmailTan35 <tan_tan.2000@hotmail.com>
2021-04-16 18:11:17 +0300

    Tiso

---
+++ ismailtan-19100001012/Assets/Script/Move1.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move1 : MonoBehaviour
+{
+    public float speed = 5;
+    [SerializeField]
+    public GameObject[] roketler;
+    public GameObject Bullet;
+    Rigidbody2D Deneme;
+    int seciliRoket = 0;
+    float atesHizi = .3f;
+    float atesZaman;
+    void Start()
+    {
+        atesZaman = atesHizi;
+    }
+
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        float z = Input.GetAxis("Rotate");
+        transform.position += new Vector3(transform.up.x * x + (transform.right.x * x), transform.up.y * y + (transform.right.y * y), 0) * speed * Time.deltaTime;
+        transform.Rotate(0, 0, -z );
+
+        if (Input.GetKeyUp(KeyCode.Joystick1Button6))
+        {
+            if (seciliRoket<2)
+            {
+                roketler[seciliRoket].transform.parent = null;
+                (roketler[seciliRoket].GetComponent<Rigidbody2D>()).simulated=true;
+                roketler[seciliRoket].GetComponent<Roket>().enabled = true;
+                seciliRoket++;
+            }         
+        }
+
+        if (Input.GetKey(KeyCode.Joystick1Button0))
+        {
+            Instantiate(Bullet, transform.position, transform.rotation);
+            if (atesZaman > 0 )
+            {
+                atesZaman = atesZaman - Time.deltaTime;
+            }
+            else
+            {
+                Instantiate(Bullet, transform.position, transform.rotation);
+                atesZaman = atesHizi;
+            }
+        }
+    }
+}
---
+++ ismailtan-19100001012/Assets/Script/Roket.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roket : MonoBehaviour
+{
+    public float speed = 5;
+    public float rotationSpeed = 1;
+    bool fire = false;
+    float sec = 5f;
+    float simSec = .5f;
+    [SerializeField]
+    Rigidbody2D Deneme;
+    void Start()
+    {
+       
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (fire==true)
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }        
+    }
+    
+    void FixedUpdate()
+    {
+        Deneme.MovePosition(transform.position + (transform.up * speed * Time.deltaTime));
+        speed += speed * Time.deltaTime;
+        speed = Mathf.Clamp(speed, 1f, 5f);
+        if (transform.rotation.eulerAngles.z < 45 || transform.rotation.eulerAngles.z > 355)
+        {
+            transform.Rotate(new Vector3(0,0,1 * rotationSpeed * Time.deltaTime));
+            rotationSpeed += rotationSpeed * Time.deltaTime;
+        }
+
+        if(sec > 0)
+        {
+            sec -= Time.deltaTime;
+            if(simSec > 0)
+            {
+                simSec-= Time.deltaTime;
+            }
+            else
+            {
+                print("sin");
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+            fire = false;
+        }
+    }
+}
